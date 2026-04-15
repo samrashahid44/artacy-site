96058e418a0b97c54d6e3cc64815e8427bf8edfe
ibtisam <ibtisam.arif@gmail.com>
2026-04-15 18:18:53 +0200

    feat: initialize App component with react-router-dom routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CaseStudies from "./pages/CaseStudies";
 import CaseStudy from "./pages/CaseStudy";
